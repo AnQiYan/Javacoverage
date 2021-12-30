8d0c799a54cbcfb13309ce2a7d52286447fb58b5
AnQiYAN <781236437@qq.com>
2021-12-30 17:26:05 +0800

    Insufficient Mul

---
+++ src/main/java/com/example/demo/MainHome/Mul.java
@@ -0,0 +1,12 @@
+package com.example.demo.MainHome;
+
+public class Mul {
+    private int a;
+    private int b;
+    private int result;
+    public Mul(int a, int b){
+        result = a +b;
+    }
+
+
+}
