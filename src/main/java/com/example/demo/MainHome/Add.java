63cd39d432e5dd08b5214ee40aaf70d31a6ece09
AnQiYAN <781236437@qq.com>
2021-12-30 17:15:09 +0800

    added all files

---
+++ src/main/java/com/example/demo/MainHome/Add.java
@@ -0,0 +1,14 @@
+package com.example.demo.MainHome;
+
+public class Add {
+    public int num1 = 0;
+    public int num2 = 0;
+    public int result = 0;
+    public Add(int num1, int num2){
+        result = num1 + num2;
+    }
+
+    public Integer getResult(){
+        return result;
+    }
+}
---
+++ src/main/java/com/example/demo/MainHome/MainTest.java
@@ -0,0 +1,10 @@
+package com.example.demo.MainHome;
+
+public class MainTest {
+    public static void main(String[] args){
+        System.out.println("Hello WORLD");
+        Add cal = new Add(3, 8);
+        System.out.println(cal.getResult());
+        System.out.println(19);
+    }
+}
